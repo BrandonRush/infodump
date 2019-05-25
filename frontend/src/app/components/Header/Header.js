3218ee794f2bd97fcd9a46ad20a7ce645c4c2d61
Brandon Rush <git@brandonrush.net>
2019-05-25 06:12:53 -0400

    Changed from class to functional component.

--- frontend/src/app/components/Header/Header.js
+++ frontend/src/app/components/Header/Header.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 
-class Header extends Component {
-  render() {
-    return (
-      <Container fluid className="p-5 mt-5 transparent nohighlight">
-        <Row>
-          <div className="col-sm text-center text-light" />
-          <div className="col-sm text-center text-light h1 font-weight-bold">
-            <div
-              href="/#"
-              role="link"
-              className="noselect canselect text-light title"
-              style={{ cursorDecoration: 'none' }}
-            >
-              <i className="icon ion-md-information-circle mr-2" />
-              infodump
-              {/* <span className="tiny-text theme-switch" role="img">
+const Header = () => {
+  return (
+    <Container fluid className="p-5 mt-5 transparent nohighlight">
+      <Row>
+        <div className="col-sm text-center" />
+        <div className="col-sm text-center h1 ">
+          <div
+            href="/#"
+            role="link"
+            className="noselect canselect text-light title"
+            style={{ cursorDecoration: 'none' }}
+          >
+            <i className="icon ion-md-information-circle mr-2" />
+            infodump
+            {/* <span className="tiny-text theme-switch" role="img">
                 🌗
               </span> */}
-            </div>
           </div>
-          <div className="col-sm text-center text-light font-weight-normal" />
-        </Row>
-      </Container>
-    );
-  }
-}
+        </div>
+        <div className="col-sm text-center text-light font-weight-normal" />
+      </Row>
+    </Container>
+  );
+};
 
 export default Header;

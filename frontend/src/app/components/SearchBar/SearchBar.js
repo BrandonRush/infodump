5266767908e8b8b4e8fc06b3b7c420c5c8fd3a6e
Brandon Rush <git@brandonrush.net>
2019-05-27 08:19:06 -0400

    Created searchbar component.

--- frontend/src/app/components/SearchBar/SearchBar.js
+++ frontend/src/app/components/SearchBar/SearchBar.js
@@ -3,16 +3,11 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <form data-js-search-form="" className="search" action="." method="get">
-        <label nesting="" id="" className="search-box ">
+      <form className="search" method="get">
+        <label className="search-bar">
           <span className="prefix">infodump.io</span>
-          <span className="sep" />
-          <input
-            name="q"
-            type="text"
-            data-js-search-input=""
-            placeholder="Search..."
-          />
+          <span className="sep">/</span>
+          <input type="text" placeholder="Search..." />
         </label>
       </form>
     );

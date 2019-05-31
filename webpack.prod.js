9a0dd23afa636d8829052126982d0f601467ff53
Brandon Rush <git@brandonrush.net>
2019-05-31 13:50:39 -0400

    Removed useless lines.

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,9 +3,6 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',

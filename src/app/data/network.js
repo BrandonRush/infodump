a83ac8267569849353c5ae901d7340ce95352460
Brandon Rush <git@brandonrush.net>
2019-05-31 13:01:56 -0400

    Switched to axios.

--- src/app/data/network.js
+++ src/app/data/network.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import { getFromServer } from './server_utils';
 
 const getIP_RTC = function() {
--- src/app/data/server_utils.js
+++ src/app/data/server_utils.js
@@ -1,8 +1,10 @@
+import axios from 'axios';
+
 export async function getFromServer(resource) {
   let response;
   try {
-    response = await fetch(resource);
-    response = response.text();
+    response = await axios(resource);
+    response = response.data;
   } catch (error) {
     console.error(error);
     response = 'Not Found';

bf68c988bd097d48792c11fbb940295ca99c6028
Brandon Rush <git@brandonrush.net>
2019-06-04 16:37:21 -0400

    Initial commit.

---
+++ src/app/components/ToolTip/ToolTip.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ToolTip() {
+  return (
+    <i
+      className="ion-md-information-circle-outline canselect"
+      role="button"
+      aria-label="ToolTip"
+    />
+  );
+}
+
+export default ToolTip;

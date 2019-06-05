8278447452d4fed87796f9e52ffec1cb5e3b870b
Brandon Rush <git@brandonrush.net>
2019-06-05 16:00:06 -0400

    Fixed static location.

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, '/build'),
+    contentBase: path.join(__dirname, '/assets'),
     watchOptions: {
       aggregateTimeout: 300,
       poll: 1000,

3fcfa4ab78c372c27ecdd2c3f3ce7fe4c8dcf3f6
Brandon Rush <git@brandonrush.net>
2019-06-06 14:55:58 -0400

    Removed bootstrap leftovers.

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -2,26 +2,15 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="container container-fluid p-5 transparent nohighlight">
-      <div className="row">
-        <div className="col-sm text-center" />
-        <div className="col-sm text-center h1 ">
-          <div
-            href="/#"
-            role="link"
-            className="noselect canselect title logo-text"
-            style={{ cursorDecoration: 'none' }}
-          >
-            <i className="icon ion-md-information-circle mr-2" />
-            <span id="info">info</span>
-            <span id="dump">dump</span>
-            {true && <span id="beta">beta</span>}
-            {/* <span className="tiny-text theme-switch" role="img">
+    <header className="transparent nohighlight">
+      <div role="link" className="noselect canselect title">
+        <i className="icon ion-md-information-circle mr-2" />
+        <span id="info">info</span>
+        <span id="dump">dump</span>
+        {false && <span id="beta">beta</span>}
+        {/* <span className="tiny-text theme-switch" role="img">
                 🌗
               </span> */}
-          </div>
-        </div>
-        <div className="col-sm text-center text-light font-weight-normal" />
       </div>
     </header>
   );
